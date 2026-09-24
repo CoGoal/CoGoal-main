8834fa0ffab8cbc8c028654b2f7c6af12fd3bac5
Andrey <valinkinandrey@gmail.com>
2026-09-25 01:03:30 +0300

    CG-1 created a skeleton and added general tables with liquibase

---
+++ src/main/java/edu/stankin/cogoalmain/CoGoalMainApplication.java
@@ -0,0 +1,13 @@
+package edu.stankin.cogoalmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoGoalMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoGoalMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/stankin/cogoalmain/CoGoalMainApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.stankin.cogoalmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoGoalMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
